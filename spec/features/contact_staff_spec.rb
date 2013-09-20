3020f1911ea4c4452016dfdd871463728512c093
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-09-20 19:16:36 -0400

    Created spec/features testing file with User story, acceptance criteria and acceptance tests.

---
+++ spec/features/contact_staff_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "user contacts DogHub", %Q{
+  As a site visitor
+  I want to contact DogHub staff
+  So that I can tell them how awesome they are
+} do 
+
+  # Acceptance Criteria
+  # * I must specify a first name
+  # * I must specify a last name
+  # * I must specify a valid email address
+  # * I must specify a subject
+  # * I must specify a description
+
+  scenario 'specifies valid information, contacts DogHub' do 
+    visit '/'
+    fill_in "First name", with: 'Steph'
+    fill_in "Last name", with: 'Viccari'
+    fill_in "Email", with: 'steph@gmail.com'
+    fill_in 'Subject', with: 'Hola'
+    fill_in 'description', with: 'You Rock'
+    expect(page).to have_content('You\'re email was successfully sent')
+  end
+end
